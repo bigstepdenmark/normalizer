df3d4135ced8dd3abd93652bea6994689c5e2981
Ismail Cam <i@bigstep.dk>
2017-11-16 14:14:23 +0100

    Install maven stub

---
+++ src/main/java/dk/cphsoftdev/app/App.java
@@ -0,0 +1,13 @@
+package dk.cphsoftdev.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
